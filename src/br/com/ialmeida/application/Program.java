616781855fa1b8d2e3a27cf1ccd2087ab2823982
Iuri Almeida <ialmeida@vertigo.com.br>
2021-12-08 15:27:12 -0300

    Implementando as classes Board e Piece

--- src/br/com/ialmeida/application/Program.java
+++ src/br/com/ialmeida/application/Program.java
@@ -1,7 +1,11 @@
 package br.com.ialmeida.application;
 
+import br.com.ialmeida.boardgame.Board;
+
 public class Program {
     public static void main(String[] args) {
+
+        Board board = new Board(3, 3);
 
     }
 }
